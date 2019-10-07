63996e8b9944e67167f9498dc168a7861c458d7d
DanielMendez <danieledmendez1990@gmail.com>
2019-10-06 20:47:24 -0700

    started on memeber class tests

--- tests/address_tests.cpp
+++ tests/address_tests.cpp
@@ -16,21 +16,21 @@ https://github.com/AlexanderJDupree/ChocAn
 #include <catch.hpp>
 #include <ChocAn/address.hpp>
 
-TEST_CASE("Constructors for address classes", "[constructors], [address]")
-{
-    SECTION("Construct valid address object")
-    {
+TEST_CASE("Constructors for address classes", "[constructors], [address]"){
+
+    SECTION("Construct valid address object"){
+
         REQUIRE(Address("line one","city","OR",97080).ok());
     }
-    SECTION("Ensures usuable information has been entered into each field")
-    {
+
+    SECTION("Ensures usuable information has been entered into each field"){
+
         REQUIRE_THROWS(Address(" ","city","OR",97080));
         REQUIRE_THROWS(Address("line one"," ","OR",97080));
         REQUIRE_THROWS(Address("line one","city"," ",97080));
         REQUIRE_THROWS(Address("line one","city","OR",0));
     }
-    SECTION("Checks that exception is thrown for invalid entries")
-    {  
+    SECTION("Checks that exception is thrown for invalid entries"){  
 
         REQUIRE_THROWS_AS(Address("extra long test string test","city","OR",97080),address_error);
         REQUIRE_THROWS_AS(Address("","","",0),address_error);
