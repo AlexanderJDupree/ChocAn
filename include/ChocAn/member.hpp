10bac37422f6ab91b7a7ddd4490488ee76c910b4
DanielMendez <danieledmendez1990@gmail.com>
2019-10-07 14:03:18 -0700

    started the header for the member class but had to stop for class

---
+++ include/ChocAn/member.hpp
@@ -0,0 +1,16 @@
+/* 
+ 
+File: member.hpp
+
+Brief: Member class packages all the personal information about a member.
+
+Authors: Daniel Mendez 
+         Alexander Salazar
+         Arman Alauizadeh 
+         Alexander DuPree
+
+https://github.com/AlexanderJDupree/
+ 
+*/
+
+#ifndef
