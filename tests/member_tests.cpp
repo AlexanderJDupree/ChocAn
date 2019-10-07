5d3bcec76523183d743336ccc5ad016d0faad307
DanielMendez <danieledmendez1990@gmail.com>
2019-10-06 20:47:24 -0700

    started on memeber class tests

---
+++ tests/member_tests.cpp
@@ -0,0 +1,39 @@
+/* 
+
+File: member_tests.cpp
+
+Brief: Unit tests for address class and associated utility classes.
+
+Authors: Daniel Mendez 
+         Alexander Salazar
+         Arman Alauizadeh 
+         Alexander DuPree
+
+https://github.com/AlexanderJDupree/ChocAn
+ 
+*/
+
+#include <catch.hpp>
+#include <ChocAn/member.hpp>
+#include <ChocAn/address.hpp>
+
+TEST_CASE("Constructors for member class", "[constructors], [member]"){
+
+    SECTION("Construct valid member object"){
+
+        Address address("street", "city", "OR", 97080);
+
+        REQUIRE(Member(address,"first name", "last name",123456789).ok());
+
+    }
+
+}    
+
+
+
+
+
+
+
+
+
