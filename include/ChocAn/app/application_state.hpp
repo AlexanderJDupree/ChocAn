e5eb73f7c25a279387e838037a90d07fcfe72b53
Alexander DuPree <alexander.j.dupree@gmail.com>
2019-11-14 14:28:13 -0800

    Added Transaction application states

--- include/ChocAn/app/application_state.hpp
+++ include/ChocAn/app/application_state.hpp
@@ -21,6 +21,8 @@ https://github.com/AlexanderJDupree/ChocAn
 #define CHOCAN_APPLICATION_STATE_H
 
 #include <variant>
+#include <ChocAn/core/entities/transaction.hpp>
+#include <ChocAn/core/utils/transaction_builder.hpp>
 
 class Login
 {
@@ -42,23 +44,26 @@ public:
     std::string status;
 };
 
-
 class Add_Transaction 
 { 
 public:
-    std::string status;
+    Transaction_Builder* builder;
 };
 
-class View_Account   { };
-class Delete_Account { };
-class Create_Account { };
-class Update_Account { };
+class Confirm_Transaction 
+{
+public: 
+    Transaction transaction;
+};
 
 using Application_State = std::variant< Login
                                       , Exit
                                       , Provider_Menu
                                       , Manager_Menu
                                       , Add_Transaction
+                                      , Confirm_Transaction
                                       >;
+
+using State_Ptr = std::unique_ptr<Application_State>;
 
 #endif // CHOCAN_APPLICATION_STATE
