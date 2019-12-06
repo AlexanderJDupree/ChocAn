3e3601bcb15574fc91941b9474205a435a22cb23
Dominique Moore <ddm4@rita-temp.cecs.pdx.edu>
2019-12-05 23:00:34 -0800

    commting files

---
+++ temp/state_controller.hpp
@@ -0,0 +1,74 @@
+/* 
+ 
+File: state_controller.hpp
+
+Brief: State Controller models user interaction with the application as a 
+       Finte State Machine. 
+
+Authors: Daniel Mendez 
+         Alex Salazar
+         Arman Alauizadeh 
+         Alexander DuPree
+         Kyle Zalewski
+         Dominique Moore
+
+https://github.com/AlexanderJDupree/ChocAn
+ 
+*/
+
+#ifndef  CHOCAN_STATE_CONTROLLER_H
+#define  CHOCAN_STATE_CONTROLLER_H
+
+#include <map>
+#include <stack>
+#include <functional>
+#include <ChocAn/core/chocan.hpp>
+#include <ChocAn/app/state_viewer.hpp>
+#include <ChocAn/app/input_controller.hpp>
+#include <ChocAn/app/application_state.hpp>
+
+class State_Controller
+{
+public:
+
+    using ChocAn_Ptr        = ChocAn::ChocAn_Ptr;
+    using Runtime_Stack     = std::stack<Application_State>;
+    using State_Viewer_Ptr  = State_Viewer::State_Viewer_Ptr;
+    using Input_Control_Ptr = Input_Controller::Input_Control_Ptr;
+    using Transition_Table  = std::map<std::string, std::function<Application_State()>>;
+
+    // TODO set default instances
+    State_Controller( ChocAn_Ptr        chocan
+                    , State_Viewer_Ptr  state_viewer
+                    , Input_Control_Ptr input_controller
+                    , Application_State initial_state = Login());
+
+    State_Controller& interact();
+
+    const Application_State& current_state() const;
+
+    bool end_state() const;
+
+    /** Visitor Methods **/
+    Application_State operator()(Exit&);
+    Application_State operator()(Login&);
+    Application_State operator()(View_Account&);
+    Application_State operator()(Find_Account&);
+    Application_State operator()(Manager_Menu&);
+    Application_State operator()(Provider_Menu&);
+    Application_State operator()(Add_Transaction&);
+    Application_State operator()(Confirm_Transaction&);
+
+private:
+
+    Application_State pop_runtime();
+
+    ChocAn_Ptr         chocan;
+    State_Viewer_Ptr   state_viewer;
+    Input_Control_Ptr  input_controller;
+    Runtime_Stack      runtime;
+    bool               is_end_state = false;
+
+};
+
+#endif // CHOCAN_STATE_CONTROLLER_H
---
+++ temp/terminal_state_viewer.cpp
@@ -0,0 +1,129 @@
+/* 
+File: terminal_state_viewer.cpp
+
+Brief:  Implementation file for terminal state viewer
+
+Authors: Daniel Mendez 
+         Alex Salazar
+         Arman Alauizadeh 
+         Alexander DuPree
+         Kyle Zalewski
+         Dominique Moore
+
+https://github.com/AlexanderJDupree/ChocAn
+ 
+*/
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <ChocAn/view/terminal_state_viewer.hpp>
+
+Terminal_State_Viewer::Terminal_State_Viewer( std::string&& view_location
+                                            , std::string&& file_extension 
+                                            , std::ostream& out_stream )
+    : view_location  ( view_location  )
+    , file_extension ( file_extension )
+    , out_stream     ( out_stream     )
+    , command_table  ( {
+          { "header",       [&](){ return render_view("header");  } }
+        , { "footer",       [&](){ return render_view("footer");  } }
+        , { "prompt",       [&](){ event_callback(); } }
+    } )
+    , resources ({})
+    {}
+
+void Terminal_State_Viewer::render_state(const Application_State& state, Callback handler)
+{
+    resources = Resource_Loader(state);
+
+    event_callback = handler;
+
+    return this->update();
+}
+
+void Terminal_State_Viewer::update()
+{
+    resources.update();
+    try
+    {
+        render_view(resources.table.at("state_name"));
+    }
+    catch(const std::out_of_range&)
+    {
+        out_stream << "Error: 'state_name' is not defined for current state\n";
+    }
+}
+
+void Terminal_State_Viewer::render_view(const std::string& view_name)
+{
+    std::string view = view_location + view_name + file_extension;
+    std::ifstream file(view);
+
+    if(file.is_open())
+    {
+        char symbol;
+        while (file.get(symbol).good())
+        {
+            // Display characters until 'start command' character is reached
+            if (symbol == '<') 
+            { 
+                if (file.peek() == '@' && file.get(symbol).good())
+                {
+                    out_stream << read_resource(read_command(file));
+                }
+                else
+                {
+                    execute_command(read_command(file)); 
+                }
+            }
+            else               
+            { 
+                out_stream << symbol; 
+            }
+        }
+    }
+    else
+    {
+        out_stream << "Error: Unable to open view: " << view << std::endl;
+    }
+    return file.close();
+}
+
+std::string Terminal_State_Viewer::read_command(std::ifstream& file)
+{
+    char symbol;
+    std::stringstream command;
+
+    // Consume symbols until end command character is reached
+    while(file.get(symbol).good() && symbol != '>')
+    {
+        command << symbol;
+    }
+    return command.str();
+}
+
+std::string Terminal_State_Viewer::read_resource(const std::string& resource_name)
+{
+    try
+    {
+        return resources.table.at(resource_name);
+    }
+    catch(const std::out_of_range&)
+    {
+        return "Unknown Resource: " + resource_name;
+    }
+}
+
+void Terminal_State_Viewer::execute_command(const std::string& command)
+{
+    try
+    {
+        command_table.at(command)();
+    }
+    catch(const std::out_of_range&)
+    {
+        out_stream << "Viewer Command: " << command << ", not recognized\n";
+    }
+    return;
+}
